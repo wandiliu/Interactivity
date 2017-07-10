c17ec1a9c060a9cd80174c15d8ba7438b2a71eb2
Wandi <lwd726@gmail.com>
2017-07-10 13:03:52 -0700

    finished project and added readme

---
+++ proj1/README.md
@@ -0,0 +1,17 @@
+### OpenWaters
+
+OpenWaters is a drawing application that was inspired by the dynamic 
+interactions between natural landscapes of the earth with water and 
+the sky. Exiting the welcome stage, this application first generates a 
+background environment that takes into account and reflects the user's 
+time of day (ie. sunset, midday, twilight, and dusk). The user is 
+limited to drawing on only the 'land' portion of the canvas, which 
+lets the user explore interactions between his/her drawings on 
+'land' and their representations in water - to show that reflections do not always mirror the landscape. Users are instructed with verbal 
+instructions before entering the game; after entering the game, however, 
+no more verbal instructions are present, and they are given 3 different
+brush options through graphic representations of the brush type, as well
+as which keys to hold to select the brush. 
+
+[Try it out here](http://alpha.editor.p5js.org/wandier/sketches/BkfJc-QEb)
+
--- proj1/sketch.js
+++ proj1/sketch.js
@@ -1,122 +1,196 @@
-/*
-initial state: setup background & variables
-*/
-
 //bool flag handles transitions
 var stopIntro = false;
-var buttonPressed = false;
-var canDraw = false;
 
 //colors
 var babyblue = '#F4FBFF'
 var teal = '#67DBF0'
-var lakeblue = '#053A55'
+var lakeblue = '#AAA9E3'
 var w = 'white'
 var salmon = '#FF9F9F'
+var c;
 
 //background setting
-var bg = lakeblue
-var h
+var bg = w
+
 
 //text setting
-var intro_text = "Welcome.\nPress your cursor to begin."
+var clear_key = "Press 'C' to clear screen"
+var save_key = "Press 'S' to save drawing"
 var btn_txt = "See Reflection"
-
+var b_btn = "Hold 'B' for bold"
+var e_btn = "Hold 'E' for ellipse"
+var r_btn = "Hold 'R' for square"
 
 function setup() {
 	createCanvas(600, 600);
-  textFont('Trebuchet MS');
+  textFont('Verdana');
   background(bg); //background color
  	h = hour()
 }
+
+//intro_text
+function intro() {
+  fill(lakeblue)
+  stroke(lakeblue)
+  if(millis() > 2000) {
+  textSize(17);
+  textAlign(CENTER);
+  text("Press your cursor to begin.", 300, 280);
+  textAlign(LEFT);
+  textSize(17);
+  text(clear_key, 50, 310);
+  text(save_key, 340, 310);
+  }
+  textAlign(CENTER);
+  textSize(32);
+  text("Welcome.\n", 300, 250);
+} 
+
 //intro special effects
 function draw_ripple() {
   noStroke();
   if(millis() > 1300) {
- 	fill('#3A6479');
-  ellipse(303, 300, 500, 500);
+ 	fill('#FFDCD8');
+  ellipse(300, 600, 500, 500);
   }
     if(millis() > 1000){
-  fill('#597C8E');
-  ellipse(303, 300, 400, 400);
+  fill('#F1AFBD');
+  ellipse(300, 600, 400, 400);
   }
     if(millis() > 700){
-  fill('#7894A3');
-  ellipse(303, 300, 300, 300);
+  fill('#D597D6');
+  ellipse(300, 600, 300, 300);
   }
   	if(millis() > 300){
-  fill('#95ABB7');
-  ellipse(303, 300, 200, 200);
+  fill('#A7AAE1');
+  ellipse(300, 600, 200, 200);
   }
   
   if(millis() > 200) {
-    fill('#B2C2CA');
-  ellipse(300, 300, 100, 100);
+    fill('#A091D6');
+  ellipse(300, 600, 100, 100);
   }
-}
-//intro_text
-function intro() {
-  if(millis() > 2000) {
-  textSize(21);
-  textAlign(CENTER);
- 	fill(lakeblue);
-  text("Press your cursor to begin.", 300, 320);
-  }
-  textSize(32);
-  textAlign(CENTER);
- 	fill(lakeblue);
-  text("Welcome.\n", 300, 300);
-}
+  
+} 
 
-//button's text
-function buttonText() {
-  textSize(21);
-  textAlign(CENTER);
- 	fill(w);
-  text(btn_txt, 300, 457);
-
-}
-//'See Reflection' Button
-function btn() {
-	fill(teal)
-  noStroke();
-  var btn = quad(150, 425, 450, 425, 450, 475, 150, 475);
-  buttonText();
+function getLighting() {
+  var i = hour(); //h:{1-24}
+  colorMode(HSB);  // Try changing to HSB.
+  var twilight = color(332, 67, 84);
+	var dawn = color(300, 12, 97);
+  var sunset = color (12, 55, 97);
+  var dusk = color(0, 100, 25);
+  var midday = color (198, 35, 100);
+  var midnight = '#00283C';
+    if(i>21 || i<5) c = midnight
+    else if (i>4 && i <10) c = lerpColor(dawn, twilight, (8-i)/4);
+    else if (i>9 && i<16) c = midday;
+    else if (i > 15 && i < 22) c = lerpColor(dusk, sunset, (22-i)/6);
+   // else c = lerpColor(sunset, midnight, (20-i)/2);
+  return c;
 }
 
-//check if mouse is in button's position
-function mouse_on_btn() {
-	if ((mouseX < 450) && (mouseY < 475) && (mouseX > 150) && (mouseY > 425)) return true;
-}
-
-
-function light() {
- background(babyblue)
+function setScreen() {
+ background(getLighting())
+  if(h>6 && h<19) {
   fill(salmon)
   noStroke()
   ellipse(60, 60, 60, 60)
-  //from = color(202, 4, 100);
-	//to = color(0, 38, 100);
-  //var c = lerpColor(to, from, 0.90);  
   fill('#D6F2EF');
  	quad(0, 300, 600, 300, 600, 600, 0, 600);
 	fill('#EDC7EA')
   ellipse(60,600-60, 60,60);
-  
-
-}
-
-function dark() {
-  background('#00283C')
+  }
+  else{
+     background(getLighting())
   fill('#FFF09F')
   noStroke()
-  ellipse(60, 60, 60, 60)
   fill('#3C2F4A');
  	quad(0, 300, 600, 300, 600, 600, 0, 600);
-	fill('#FDFFC9')
-  ellipse(60, 600-60, 60,60);
+  for (var i = 0; i < 60; i++) {
+  	var px = random(599);
+  	var py = random(0, 120);
+  	var r=random(2,5);
+    fill('#FDFFC9')
+		ellipse(px, py, r, r);
+    ellipse(px, 600-py, r, r);
+  }
+}
+    fill(getLand())
+  	textAlign(LEFT);
+  	textSize(20)
+  	strokeWeight(30);
+    ellipse(266, 43, 30, 30)
+  	fill('black');
+  	text('d', 260, 50);
+    fill(getLand())
+    text('e', 300, 50);
+    text('r', 340, 50);
+  	noFill();
+  	stroke(getLand())
+  	strokeWeight(1);
+  	ellipse(306, 44, 20, 20)
+  	rect(331, 31, 25, 25);
+}
 
-	
+
+function mouseClicked() {
+  if (stopIntro == false) {
+  stopIntro = true;
+  setScreen();
+  }
+}
+
+function keyTyped() {
+	switch(key){
+    case 'c':
+      setScreen();
+      break;
+    case 's':
+      saveCanvas('OpenWaters','.png')
+      break;
+    default:
+      strokeWeight(1);
+            }}
+
+
+function draw() {
+ if(!stopIntro){ //before intro is stopped, draw intro
+   draw_ripple();
+   intro();
+  }
+if(stopIntro==true && mouseIsPressed && mouseInRange()){
+
+  switch(key){
+    case 'd':
+      noStroke();
+      fill(getLand());
+  		ellipse(mouseX, mouseY, 30, 30);
+  		fill(getWater())
+  		ellipse(pmouseX-5, 600-pmouseY, 15, 35)
+ 		 break;
+    case 'e':
+      noFill()
+      stroke(getLand());
+  		ellipse(mouseX, mouseY+10, 20, 20);
+  		stroke(getWater())
+  		ellipse(pmouseX-15, 600-pmouseY, 10, 10)
+      break;
+    case 'r':
+      noFill()
+      stroke(getLand());
+  		rect(mouseX-12, mouseY-12, 25, 25);
+  		stroke(getWater())
+  		rect(pmouseX-10, 600-pmouseY-15, 20, 12)
+      break;
+    default:
+  stroke(getWater());
+  line(pmouseX-5, (600-pmouseY)*1.05, mouseX+3, (600-mouseY)*1.05);
+  stroke(getLand())
+  noFill();
+  line(pmouseX, pmouseY, mouseX, mouseY)
+            }
+}
 }
 
 function getLand() {
@@ -128,78 +202,8 @@ function getWater() {
  	if(h>6 && h<19) return ('#88CE7B')
   else return ('#19E2BB')
 }
-//handles mouse operations 
-function mouseClicked() {
-  if (stopIntro == false) {
-  stopIntro = true;
-  canDraw = true;
-  if(h>6 && h<19)light();
-  else dark()
-  strokeWeight(4)
-	line(0, 300, 600, 300);
-  
-  }
-  
-  if(mouse_on_btn()==true){ //if mouse clicked on the button
-    opa=0;
-    buttonPressed=true; //button was pressed
-    console.log("button pressed")
-    canDraw=false;
-  }
 
-}
 
-function draw_rect() {
-
-  
-}
-/*
-
-function draw() { 
-
-  
-  if(stopIntro){
-  	background(babyblue);
-  }
-  
-  if(!buttonPressed && stopIntro) { //btn(); 
-    art();} //before the button is pressed, button remains there
-	
-  
-}
-
-*/
-
-function draw() {
- if(!stopIntro){ //before intro is stopped, draw intro
-  	draw_ripple();
-       intro();
-  }
-  
-if (stopIntro==true && !buttonPressed) {
-  noStroke();
-//  fill(255, 255, 255, opa);  
- // quad(0, 300, 600, 300, 600, 600, 0, 600);
-  //console.log("made a rectangel"); 
-  //btn();
-}
-  
 function mouseInRange() {
  return ((mouseX < 600) && (mouseY < 300) && (mouseX > 0) && (mouseY > 0))
-}
-  
-if(stopIntro==true && mouseIsPressed && mouseInRange()){
-   //stroke('black');
-   //strokeWeight(2);
-   //line(pmouseX, pmouseY, mouseX, mouseY);
-  stroke(getWater());
-  strokeWeight(1);
-  line((pmouseX+5)*1.3, 600-pmouseY, (mouseX)*1.2, 600-mouseY);
-  stroke(getLand())
-  noFill();
-  strokeWeight(1)
-  ellipse(mouseX, mouseY, 15, 15)
-
-}
-  
 }
